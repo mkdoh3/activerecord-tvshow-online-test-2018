e607f656c593413abaf82d37421eed3b40a2f4b8
mkdoh3 <mkdoherty3@gmail.com>
2019-07-30 21:44:35 -0500

    student examples

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,17 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 
   def self.most_popular_show
-    self.find
+    self.where('rating = ?', self.highest_rating).first
+  end
+  def self.least_popular_show
+    self.where('rating = ?', self.lowest_rating).first
+  end
+  def self.rating_sum
+    self.sum(:rating)
   end
 end
